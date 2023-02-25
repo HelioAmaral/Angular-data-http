94552df3454f59c09e5deaabe771e6fac4d8fc6a
Helio Amaral <helio.amaral81@gmail.com>
2023-02-25 09:29:38 +0100

    input handle

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent {
+//the term the user types in the search bar, set to empty string, any type the user changes that input, we will take the value of that input and use it to update the property term 
+  term = '';
+
+  //on the callback method onInput we take the value as a parameter and we use it to update the term property
+  /*
+  onInput(value: string){
+    this.term = value; 
+  }*/
 
 }
